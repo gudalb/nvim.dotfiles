782a2654428bec51344c91fadee4e6ae53954955
Albin Gudmundsson <albin.gudmundsson@nionit.com>
2025-07-03 22:02:16 +0200

    reset to only roslyn

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-require("neotest").setup({
-    adapters = {
-        require("neotest-dotnet"),
-    },
+require("mason").setup({
+  registries = {
+    "github:mason-org/mason-registry",
+    "github:Crashdummyy/mason-registry",
+  },
 })
-
-require("go").setup()
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "c_sharp",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "csharpier",
+      },
+    },
+  },
+  {
+    "seblyng/roslyn.nvim",
+    ft = "cs",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
+}
