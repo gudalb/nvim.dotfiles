c38589dc13d5d1225240cf8c3d822667c06b2f22
Albin Gudmundsson <albin.gudmundsson@gmail.com>
2025-08-05 18:11:48 +0200

    added buffer tab keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,12 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Open diagnostic float" })
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-q>", function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  -- Switch to alternate buffer (last opened)
+  vim.cmd("buffer #")
+  -- Delete the previous buffer
+  vim.api.nvim_buf_delete(current_buf, { force = false })
+end, { desc = "Close buffer and go to last opened" })
