63ce77a6362b7e4d4cb7bfb64d8c91e401e8801a
Albin Gudmundsson <albin.gudmundsson@nordicstation.com>
2025-08-05 17:33:35 +0200

    added expand diagnostics box & nuget plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -31,4 +31,17 @@ return {
       -- your configuration comes here; leave empty for default settings
     },
   },
+  {
+    "MonsieurTib/neonuget",
+    config = function()
+      require("neonuget").setup({
+        -- Optional configuration
+        dotnet_path = "dotnet", -- Path to dotnet CLI
+        default_project = nil, -- Auto-detected, or specify path like "./MyProject/MyProject.csproj"
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
